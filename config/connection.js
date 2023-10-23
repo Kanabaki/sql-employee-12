5ea873383f37f0a481ee14dd6f432b7a4bd4b468
Kanabaki <Kagomekanabaki@gmail.com>
2023-10-23 13:09:58 -0500

    addEmployee func added & modularized the connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,16 @@
-// const mysql = require('mysql2');
-// const Sequelize  = require("sequelize");
-// require("dotenv").config();
+const mysql = require('mysql2');
+require("dotenv").config();
 
 
-// const sequelize = new Sequelize(
-//     // console.log(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD),
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       // Database location
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'employee_db'
+  },
+  console.log(`Connected to the mysql.db`)
+)
   
 // const db = mysql.createConnection(
 //   {
@@ -29,4 +25,4 @@
 // )
 
   // module.exports = sequelize;
-  // module.exports = db;
+  module.exports = db;
